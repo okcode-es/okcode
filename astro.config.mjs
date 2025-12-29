5034b7495b53b916fc53d86b1dd37e93b1f9f7e0
okcode <lwh7749@outlook.com>
2025-12-29 19:44:30 +0100

    New features

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,39 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightDocSearch from '@astrojs/starlight-docsearch';
+import starlightScrollToTop from 'starlight-scroll-to-top';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://okcode.es',
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			defaultLocale: 'root',
+			locales: {
+				root: { label: '简体中文', lang: 'zh-CN' },
+				en: { label: 'English', lang: 'en' },
+				es: { label: 'Español',lang: 'es' },
+			},
+			title: 'okcode',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/okcode-es' },
+			{ icon: 'twitter', label: 'Twitter', href: 'https://twitter.com/okcode_' }
+			],
 			sidebar: [
 				{
 					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					autogenerate: { directory: 'guides' },
 				},
 				{
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
 				},
+			],
+			plugins: [
+				starlightDocSearch({
+				appId: 'RGT6K369RP',
+				apiKey: '5547540b5fb5ecae5c37cb6f142de6a5',
+				indexName: 'okcode',
+				}),
+				starlightScrollToTop(),
 			],
 		}),
 	],
